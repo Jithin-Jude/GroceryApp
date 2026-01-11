d7ff433c37ba2bac4b48ca99d0af64f07e7a867a
Jithin/Jude <jithin.jude68@gmail.com>
2026-01-11 11:38:26 +0530

    7 days cache.

--- app/src/main/java/com/jithin/groceryapp/network/CacheInterceptor.kt
+++ app/src/main/java/com/jithin/groceryapp/network/CacheInterceptor.kt
@@ -19,7 +19,7 @@ class CacheInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val response: Response = chain.proceed(chain.request())
         val cacheControl = CacheControl.Builder()
-            .maxAge(10, TimeUnit.DAYS)
+            .maxAge(7, TimeUnit.DAYS)
             .build()
         return response.newBuilder()
             .header("Cache-Control", cacheControl.toString())
